c7cf4ec56f5bb233ac99c211c0d795a4b98c776c
thurunuym <marasinghe.ty@gmail.com>
2026-05-25 03:06:43 +0530

    cors issue fixed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 const allowedOrigins = [
   'http://localhost:5173', 
-  'https://issue-tracker-gamma-lime.vercel.app' // NO trailing slash at the end!
+  'https://issue-tracker-gamma-lime.vercel.app'
 ];
 
 app.use(cors({
